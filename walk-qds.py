f37bb67a8fbb5be9e3f220afd9ad8f47a01339ed
Chris Lalancette <clalancette@openrobotics.org>
2021-09-08 20:54:37 +0000

    Rewrite to use sub-commands.
    This fits the usage model must better.
    
    Signed-off-by: Chris Lalancette <clalancette@openrobotics.org>

--- walk-qds.py
+++ walk-qds.py
@@ -21,6 +21,9 @@ import sys
 import lxml.etree
 
 
+quality_level_re = re.compile(r'.*claims to be in the \*\*Quality Level ([1-5])\*\*')
+
+
 class Package:
     """Class to represent one package in the hierarchy of packages."""
 
@@ -34,40 +37,7 @@ class Package:
         self.children = []
 
 
-def main():
-    parser = argparse.ArgumentParser(description='Utility to find and print quality levels of a ROS package and dependencies')
-    parser.add_argument(
-        '--recurse',
-        help='Whether to recursively find quality levels for all dependencies',
-        action='store_true',
-        default=False)
-    parser.add_argument(
-        '--exclude',
-        help='Package to specifically exclude from quality level checking (may be passed more than once)',
-        action='append',
-        default=[])
-    parser.add_argument(
-        '--include-build-deps',
-        help='Whether to include build dependencies',
-        action='store_true',
-        default=False)
-    group = parser.add_mutually_exclusive_group()
-    group.add_argument(
-        '--package',
-        help='The top-level package for which to find quality level of dependencies',
-        action='store')
-    group.add_argument(
-        '--count',
-        help='Count the number of packages in each quality level (cannot be combined with --package)',
-        action='store_true',
-        default=False)
-    parser.add_argument(
-        'source_path',
-        help='The top-level of the source tree in which to find package and dependencies',
-        action='store')
-    args = parser.parse_args()
-
-    source_path = args.source_path
+def print_package_levels(args, package_name_to_package):
     package_name_to_examine = args.package
 
     if package_name_to_examine in args.exclude:
@@ -78,6 +48,136 @@ def main():
     if args.include_build_deps:
         dep_tags_to_consider.append('build_depend')
 
+    if package_name_to_examine not in package_name_to_package:
+        print("Could not find package to examine '%s'" % (package_name_to_examine))
+        return 2
+
+    # Starting with the package given by the user on the command-line, walk the
+    # package dependencies in a breadth-first manner.  We want breadth-first so
+    # that if a dependency shows up on more than one "level", we'll only show
+    # it at the highest level it is a dependency at.
+
+    packages_to_examine = collections.deque([package_name_to_examine])
+    depnames_found = [package_name_to_examine]
+    deps_not_found = set()
+    while packages_to_examine:
+        package = package_name_to_package[packages_to_examine.popleft()]
+        for child in package.lxml_tree.getroot().getchildren():
+            if child.tag not in dep_tags_to_consider:
+                continue
+
+            depname = child.text
+            if depname in depnames_found:
+                continue
+
+            if depname in args.exclude:
+                continue
+
+            depnames_found.append(depname)
+
+            if depname in package_name_to_package:
+                package_name_to_package[depname].depth = package.depth + 1
+                package.children.append(package_name_to_package[depname])
+                if args.recurse:
+                    packages_to_examine.append(depname)
+            else:
+                deps_not_found.add(depname)
+
+    if deps_not_found:
+        print("WARNING: Could not find dependencies '%s', skipping" % (', '.join(deps_not_found)))
+
+    # Now start walking in a depth-first search, starting from the top element,
+    # figuring out the quality levels as we go.  Note that we don't do the
+    # printing here, just so that we collect all of the WARNINGS before we
+    # print out the entire tree.
+    deps_to_print = collections.deque([package_name_to_package[package_name_to_examine]])
+    strings_to_print = []
+    while deps_to_print:
+        package = deps_to_print.popleft()
+        if not os.path.exists(package.qd_path):
+            print("WARNING: Could not find quality declaration for package '%s', skipping" % (package.name))
+            continue
+        with open(package.qd_path, 'r') as infp:
+            for line in infp:
+                match = re.match(quality_level_re, line)
+                if match is None:
+                    continue
+                groups = match.groups()
+                if len(groups) != 1:
+                    continue
+                strings_to_print.append('%s%s: %d' % ('  ' * package.depth, package.name, int(groups[0])))
+                break
+            else:
+                print("WARNING: Could not find quality level for package '%s', skipping" % (package.name))
+
+        deps_to_print.extendleft(package.children)
+
+    [print(s) for s in strings_to_print]
+
+
+def count_package_levels(args, package_name_to_package):
+    ql_totals = {}
+    for package in package_name_to_package.values():
+        if not os.path.exists(package.qd_path):
+            print("WARNING: Could not find quality declaration for package '%s', skipping" % (package.name))
+            continue
+        with open(package.qd_path, 'r') as infp:
+            for line in infp:
+                match = re.match(quality_level_re, line)
+                if match is None:
+                    continue
+                groups = match.groups()
+                if len(groups) != 1:
+                    continue
+                ql = int(groups[0])
+                if ql in ql_totals:
+                    ql_totals[ql] += 1
+                else:
+                    ql_totals[ql] = 1
+                break
+            else:
+                print("WARNING: Could not find quality level for package '%s', skipping" % (package.name))
+
+    print("# Package counts by quality level:")
+    for level in sorted(ql_totals.keys()):
+        print("%d : %d" % (level, ql_totals[level]))
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Utility to find and print quality levels of a ROS package and dependencies')
+    parser.add_argument(
+        'source_path',
+        help='The top-level of the source tree in which to find package and dependencies',
+        action='store')
+    subparsers = parser.add_subparsers(help='help for subcommand')
+
+    count_parser = subparsers.add_parser('count', help='Count the number of packages in each quality level')
+    count_parser.set_defaults(func=count_package_levels)
+
+    package_parser = subparsers.add_parser('package', help='Print the quality level of a package and its dependencies')
+    package_parser.add_argument(
+        '--recurse',
+        help='Whether to recursively find quality levels for all dependencies',
+        action='store_true',
+        default=False)
+    package_parser.add_argument(
+        '--exclude',
+        help='Package to specifically exclude from quality level checking (may be passed more than once)',
+        action='append',
+        default=[])
+    package_parser.add_argument(
+        '--include-build-deps',
+        help='Whether to include build dependencies',
+        action='store_true',
+        default=False)
+    package_parser.add_argument(
+        '--package',
+        help='The top-level package for which to find quality level of dependencies',
+        action='store',
+        required=True)
+    package_parser.set_defaults(func=print_package_levels)
+    args = parser.parse_args()
+
     # Walk the entire source_path passed in by the user, looking for all of the
     # package.xml files.  For each of them we parse the package.xml, and go
     # looking for the name of the package.  We then save that off, along with
@@ -87,7 +187,7 @@ def main():
     # foolproof method to get the proper package names.
 
     package_name_to_package = {}
-    for (dirpath, dirnames, filenames) in os.walk(source_path):
+    for (dirpath, dirnames, filenames) in os.walk(args.source_path):
         if 'package.xml' not in filenames:
             continue
 
@@ -102,104 +202,7 @@ def main():
                 tree)
             break
 
-    quality_level_re = re.compile(r'.*claims to be in the \*\*Quality Level ([1-5])\*\*')
-
-    # Were we asked to research a particlar package?
-    if package_name_to_examine:
-        if package_name_to_examine not in package_name_to_package:
-            print("Could not find package to examine '%s'" % (package_name_to_examine))
-            return 2
-
-        # Starting with the package given by the user on the command-line, walk the
-        # package dependencies in a breadth-first manner.  We want breadth-first so
-        # that if a dependency shows up on more than one "level", we'll only show
-        # it at the highest level it is a dependency at.
-
-        packages_to_examine = collections.deque([package_name_to_examine])
-        depnames_found = [package_name_to_examine]
-        deps_not_found = set()
-        while packages_to_examine:
-            package = package_name_to_package[packages_to_examine.popleft()]
-            for child in package.lxml_tree.getroot().getchildren():
-                if child.tag not in dep_tags_to_consider:
-                    continue
-
-                depname = child.text
-                if depname in depnames_found:
-                    continue
-
-                if depname in args.exclude:
-                    continue
-
-                depnames_found.append(depname)
-
-                if depname in package_name_to_package:
-                    package_name_to_package[depname].depth = package.depth + 1
-                    package.children.append(package_name_to_package[depname])
-                    if args.recurse:
-                        packages_to_examine.append(depname)
-                else:
-                    deps_not_found.add(depname)
-
-        if deps_not_found:
-            print("WARNING: Could not find dependencies '%s', skipping" % (', '.join(deps_not_found)))
-
-        # Now start walking in a depth-first search, starting from the top element,
-        # figuring out the quality levels as we go.  Note that we don't do the
-        # printing here, just so that we collect all of the WARNINGS before we
-        # print out the entire tree.
-        deps_to_print = collections.deque([package_name_to_package[package_name_to_examine]])
-        strings_to_print = []
-        while deps_to_print:
-            package = deps_to_print.popleft()
-            if not os.path.exists(package.qd_path):
-                print("WARNING: Could not find quality declaration for package '%s', skipping" % (package.name))
-                continue
-            with open(package.qd_path, 'r') as infp:
-                for line in infp:
-                    match = re.match(quality_level_re, line)
-                    if match is None:
-                        continue
-                    groups = match.groups()
-                    if len(groups) != 1:
-                        continue
-                    strings_to_print.append('%s%s: %d' % ('  ' * package.depth, package.name, int(groups[0])))
-                    break
-                else:
-                    print("WARNING: Could not find quality level for package '%s', skipping" % (package.name))
-
-            deps_to_print.extendleft(package.children)
-
-        [print(s) for s in strings_to_print]
-
-    # Or was no package name given, in which case we'll just print total
-    # counts by quality level?
-    elif args.count:
-        ql_totals = {}
-        for package in package_name_to_package.values():
-            if not os.path.exists(package.qd_path):
-                print("WARNING: Could not find quality declaration for package '%s', skipping" % (package.name))
-                continue
-            with open(package.qd_path, 'r') as infp:
-                for line in infp:
-                    match = re.match(quality_level_re, line)
-                    if match is None:
-                        continue
-                    groups = match.groups()
-                    if len(groups) != 1:
-                        continue
-                    ql = int(groups[0])
-                    if ql in ql_totals:
-                        ql_totals[ql] += 1
-                    else:
-                        ql_totals[ql] = 1
-                    break
-                else:
-                    print("WARNING: Could not find quality level for package '%s', skipping" % (package.name))
-
-        print("# Package counts by quality level:")
-        for level in sorted(ql_totals.keys()):
-            print("%d : %d" % (level, ql_totals[level]))
+    args.func(args, package_name_to_package)
 
     return 0
 
